3f9e2605034daf5a53cad719b3fc17d2e18c2017
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-06-06 16:51:45 -0600

    finish html templates

--- app/models/Jot.js
+++ app/models/Jot.js
@@ -58,4 +58,41 @@ export class Jot {
       </div>
     `
   }
+
+  get activeCardTemplate() {
+    return `
+      <i class="mdi mdi-bookmark" style="position:absolute; font-size: 6rem; margin-top: -36px; color: #ff002f"></i>
+      <div class="row px-1 mb-2">
+        <h5 class="col-9 active-jot-title mt-4 fs-3">JOT TITLE</h5>
+        <div class="col-6 time-subheader" style="color: #444798">
+          <h6>dafdfadfa</h6>
+          <h6>adlfadfkladf</h6>
+        </div>
+        <div class="col-4 mt-3">
+          <div class="row justify-content-end">
+            <button class="btn rounded col-md-4 order-2 order-md-1 text-danger border border-danger" style="background-color: #1F1D38;">
+              DELETE <i class="mdi mdi-trash-can-outline"></i>
+            </button>
+            <button class="btn btn-success col-md-4 order-1 order-md-2 ms-1" style="background-color: #1e8da6;">
+              SAVE <i class="mdi mdi-content-save-plus"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col">
+          <div class="mb-3 mx-4">
+            <form>
+              <textarea class="form-control text-light" name="body" id="body-text" rows="20" placeholder="Write Your Note Here!!!" style="background-color:#2a244d; border: 1px solid #444798;"></textarea>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-end">
+        <div class="col-5 text-end me-5 mb-3">
+          109 Words
+        </div>
+      </div>
+    `
+  }
 }
